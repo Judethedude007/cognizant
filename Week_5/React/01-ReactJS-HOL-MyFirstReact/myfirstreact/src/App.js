ca31e6c4bc581601a0cf768bf11c630ee2cdbbe5
Jude Thomas <judethomas2004@gmail.com>
2026-07-03 10:40:37 +0530

    Complete React my first app exercise

---
+++ Week_5/React/01-ReactJS-HOL-MyFirstReact/myfirstreact/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the first session of React</h1>
+      <p>This is my first React application.</p>
+    </div>
+  );
+}
+
+export default App;
